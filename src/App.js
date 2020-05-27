ffae67d7d8a38f958a8cf24b9f7c396175c422f6
unknown <leongit1990@gmail.com>
2020-05-27 02:07:43 +0200

    Finished the search by transaction functionality

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
  const [longitudCadena, setLongitudCadena] = useState(0);
  const [blockSearch, setBlockSearch] = useState("");
  const [searchResult, setSearchResult] = useState("");
+ const [transactionSearch, setTransactionSearch] = useState("");
 
 
 
@@ -27,9 +28,16 @@ const App = () => {
     })
   }, []);
 
+  let foundBlock;
+
+  useEffect(() => {
+    if(searchResult === "Encontrado"){
+      console.log("holahola")
+    }
+  }, [searchResult])
 
 
-
+ 
 
  
   socket.on('updateChain', (data) => {
@@ -45,29 +53,41 @@ const App = () => {
 
   const handleChange = (evt) => {
    setBlockSearch(evt.target.value);
-   console.log(blockSearch)
   }
 
 
 
   const submit = (evt) => {
     evt.preventDefault();
+
     
-    if(isNaN(parseInt(blockSearch))){
+    if(isNaN(blockSearch) && blockSearch.length > 40){
       for(let i = 0; i < blockChain.length; i++){
         if(blockSearch === blockChain[i].hash){
-          setSearchResult(blockChain[i].timestamp);
+          setSearchResult(blockChain[i]);
           break;
         } else {
-          setSearchResult("No se ha encontrado el bloque");
+          setSearchResult("no results");
         }
       }
-    } else {
+    } else if (isNaN(blockSearch) && blockSearch.length < 39) {
+      setBlockSearch(blockSearch + " ")
+      for(let i = 0; i < blockChain.length; i++){
+
+        for(let p = 0; p < blockChain[i].transactions.length; p++){
+          if(blockSearch === blockChain[i].transactions[p].transactionId){
+            setTransactionSearch(blockChain[i].transactions[p])
+            break;
+          }
+        }
+      }
+    } 
+    else {
       let index = parseInt(blockSearch);
       if(index < blockChain.length){
-        setSearchResult(blockChain[blockSearch].timestamp);
+        setSearchResult(blockChain[blockSearch]);
       } else {
-        setSearchResult("No se ha encontrado tal bloque")
+        setSearchResult("no results")
       }
 
     }
@@ -90,9 +110,39 @@ const App = () => {
         <button className="blockFinder">Buscar</button>
       </form>
 
-      <h1 class="blockFinderTitle">BLOCK FINDER</h1>
-      <div class="blockFinderContainer">
-        <h1>{searchResult}</h1>
+      <h1 className="blockFinderTitle">BLOCK FINDER</h1>
+      <div className="blockFinderContainer">
+        <div className="finderViewer">
+          <div>
+            <h3 className="viewerTitle">BLOCK INFO</h3>
+            <h3 className="noResults">{searchResult === "no results" ? "NO SE HAN ENCONTRADO RESULTADOS" : ""}</h3>    
+            {searchResult.index ?  <p><span>Index:</span> {searchResult.index}</p> : "" }
+            {searchResult.timestamp ? <p><span>Fecha:</span> {new Date(searchResult.timestamp).toString()}</p> : "" }
+            {searchResult.hash ? <p><span>Hash:</span> {searchResult.hash}</p> : ""}
+            {searchResult.previousBlockHash ? <p><span>Previous Block Hash:</span> {searchResult.previousBlockHash}</p> : ""}
+          </div>
+          <div>
+          <h3 className="viewerTitle">TRANSACTIONS</h3>
+
+                   {searchResult.transactions ?     <div>    {searchResult.transactions.map((trans, index2) => {
+                                return <div key={index2} className="transContainer">                                  
+                                    <p className="transactionNumber"><span>Transaction: </span>: {index2 + 1}</p>
+                                    <p><span>Transaction ID: </span> {trans.transactionId}</p>
+                                    <div className="transInfo"><p><span>Hospital: </span> {trans.hospital}</p> <p><span>Country:</span> {trans.country}</p></div>
+                                    <p className="patient"><span>Patient symptoms: </span> {trans.symptoms}</p>
+                                    <p className="patient"><span>Patient info: </span> {trans.patientInfo}</p>
+                                    </div>
+                        })} </div> : "" }
+
+                  {transactionSearch.transactionId ? <p><span>Transaction Id: </span> {transactionSearch.transactionId}</p> : ""}
+                  {transactionSearch.hospital ? <p><span>Hospital: </span> {transactionSearch.hospital}</p> : ""}
+                  {transactionSearch.country ? <p><span>Country: </span> {transactionSearch.country}</p> : ""}
+                  {transactionSearch.patientInfo ? <p><span>Patient info: </span> {transactionSearch.patientInfo}</p> : ""}
+                  {transactionSearch.symptoms ? <p><span>Symptoms: </span> {transactionSearch.symptoms}</p> : ""}
+
+         
+          </div>                     
+        </div>
       </div>
 
       <Blocks leonchain={blockChain} />
--- src/components/FinderClick.js
+++ src/components/FinderClick.js
@@ -24,8 +24,23 @@ const FinderClick = (props) => {
 
         if(finder != undefined){
             myComponent = blocks.leonchain.map((blck, indx) => {
-
-                if(indx === finder){
+                if(indx === finder && indx == 0){
+                    return <div key={indx} className="finderViewer">
+                                <div>
+                                    <h3 className="viewerTitle">BLOCK INFO</h3>
+                                    <p><span>Index:</span> {blck.index}</p>
+                                    <p><span>Fecha:</span> {new Date(blck.timestamp).toString()}</p>
+                                    <p><span>Nonce:</span> {blck.nonce}</p>
+                                    <p><span>Hash:</span> {blck.hash}</p>
+                                    <p><span>Previous Block Hash:</span> {blck.previousBlockHash}</p>
+                                </div>
+                                <div>
+                                    <h1>GENESIS BLOCK</h1>
+                                    <p>{blck.transactions[0]}</p>
+                                </div>
+                            </div>
+                }
+                else if(indx === finder && indx != 0){
                 return <div key={indx} className="finderViewer">
                         <div>
                             <h3 className="viewerTitle">BLOCK INFO</h3>
@@ -39,15 +54,18 @@ const FinderClick = (props) => {
                         <div>
                             <h3 className="viewerTitle">TRANSACTIONS</h3>
                             {blck.transactions.map((trans, index2) => {
-                                return <p key={index2}>{trans.country} </p>
+                                return <div key={index2} className="transContainer">                                  
+                                    <p className="transactionNumber"><span>Transaction: </span>: {index2 + 1}</p>
+                                    <p><span>Transaction ID: </span> {trans.transactionId}</p>
+                                    <div className="transInfo"><p><span>Hospital: </span> {trans.hospital}</p> <p><span>Country:</span> {trans.country}</p></div>
+                                    <p className="patient"><span>Patient symptoms: </span> {trans.symptoms}</p>
+                                    <p className="patient"><span>Patient info: </span> {trans.patientInfo}</p>
+                                    </div>
                         })}
-                        </div>
-                        
+                        </div>                     
                     </div>
                 }
             })
-
-            console.log(myComponent)
         }
 
 
